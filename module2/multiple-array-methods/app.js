fb8c09689e91bf40cbabd3dcf7ec06ca62c7644e
Ashley Stanley <astanley006@gmail.com>
2020-08-03 20:19:53 -0400

    finally finished mult-arr-methods

--- module2/multiple-array-methods/app.js
+++ module2/multiple-array-methods/app.js
@@ -37,40 +37,40 @@ const peopleArray = [
     }
 ]
 
-// // Returns a list of everyone older than 18, which is
-// // sorted alphabetically by last name, and where
-// // each name and age is embedded in a string that looks like an HTML <li> element.
+// // // Returns a list of everyone older than 18, which is
+// // // sorted alphabetically by last name, and where
+// // // each name and age is embedded in a string that looks like an HTML <li> element.
 
-// /*
-// Output: 
-// [ 
-//     "<li>Kyle Mooney is 27</li>",
-//     "<li>Sarah Palin is 47</li>",
-//     "<li>Rick Sanchez is 78</li>",
-//     "<li>Morty Smith is 29</li>",
-//     "<li>Lev Tolstoy is 82</li>" 
-// ]
-// */
+// // /*
+// // Output: 
+// // [ 
+// //     "<li>Kyle Mooney is 27</li>",
+// //     "<li>Sarah Palin is 47</li>",
+// //     "<li>Rick Sanchez is 78</li>",
+// //     "<li>Morty Smith is 29</li>",
+// //     "<li>Lev Tolstoy is 82</li>" 
+// // ]
+// // */
 
 function sortedOfAge(peopleArray) {
     return peopleArray.filter(person => person.age > 18)
 } 
-console.log(sortedOfAge(peopleArray))
+// console.log(sortedOfAge(peopleArray))
 
 function alphabetically(peopleArray) {
     // Code that alphabetizes here
-    peopleArray.sort((persona, personb) => {persona.lastName.localeCompare(personb.lastName) - personb.lastName.localeCompare(persona.lastName)
-    })    
+    let alphabetizes =  peopleArray.sort((persona, personb) => persona.lastName.localeCompare(personb.lastName))
+    return alphabetizes
 } 
 
-console.log(alphabetically(peopleArray))
+// console.log(alphabetically(peopleArray))
 
 function addHTML(peopleArray) {
     let html = peopleArray.map(person => "<li>" + person.firstName + " " + person.lastName + " is " + person.age + "</li>")
     return html
 } 
-    console.log(addHTML(peopleArray))
+    // console.log(addHTML(peopleArray))
 
 function allFunctions(peopleArray) {
-    return sortedOfAge(peopleArray), addHTML(peopleArray), alphabetically(peopleArray)
+    return sortedOfAge(peopleArray), alphabetically(peopleArray), addHTML(peopleArray);
 } console.log(allFunctions(peopleArray))
